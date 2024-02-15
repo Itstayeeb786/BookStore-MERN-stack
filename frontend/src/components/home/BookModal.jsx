41d825f8bb675556ab471fc94c373037b1348772
itstayeeb786 <sonoftayeb@gmail.com>
2024-02-15 08:44:03 +0300

    Initial commit

---
+++ frontend/src/components/home/BookModal.jsx
@@ -0,0 +1,46 @@
+import { AiOutlineClose } from "react-icons/ai";
+import { PiBookOpenTextLight } from "react-icons/pi";
+import { BiUserCircle } from "react-icons/bi";
+
+const BookModal = ({ book, onClose }) => {
+  return (
+    <div
+      className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
+      onClick={onClose}
+    >
+      <div
+        onClick={(event) => event.stopPropagation()}
+        className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative"
+      >
+        <AiOutlineClose
+          className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
+          onClick={onClose}
+        />
+        <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
+          {book.publishYear}
+        </h2>
+        <h4 className="my-2 text-gray-500">{book._id}</h4>
+        <div className="flex justify-start items-center gap-x-2">
+          <PiBookOpenTextLight className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book.title}</h2>
+        </div>
+        <div className="flex justify-start items-center gap-x-2">
+          <BiUserCircle className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book.author}</h2>
+        </div>
+        <p className="mt-4">A Brief Description</p>
+        <p className="my-2">
+          Roger Fritz has devoted his career to helping people overcome
+          obstacles and achieve their goals by changing their outlook on life.
+          In this invaluable book, he shows readers how to develop a deeper
+          understanding of the effects of their attitude--including how attitude
+          influences not only their performance but the results others
+          achieve--and make a change to experience the benefits of increased
+          positivity.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BookModal;
